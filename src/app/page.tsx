6c13728e0f2ea7eb944b8bf26cde84b7550eb86d
Qusai Fares <qusaifares@gmail.com>
2023-10-23 13:35:35 -0700

    Add cache time based revalidation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import styles from './page.module.scss'
 import PlayerTable from '@/components/Table/PlayerTable/PlayerTable'
 import { LeaderboardApi } from '@bakery-leaderboard-ts-sdk'
+import { Configuration } from '../../resources/BakeryLeaderboardServiceModel/output/model/typescript';
 
 export default async function Home() {
-  const leaderboardApi = new LeaderboardApi();
+  const leaderboardApi = new LeaderboardApi(new Configuration({
+    fetchApi(input, init) {
+        return fetch(input, { ...init, next: { revalidate: 60 } });
+    },
+  }));
 
   const leaderboard = await leaderboardApi.getLeaderboard();
 
