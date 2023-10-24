2d966733e443adfbf4e84cafb8025f15a4da87ad
Qusai Fares <qusaifares@gmail.com>
2023-10-24 15:18:41 -0700

    Remove revalidate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,8 @@ export default async function Home() {
         return fetch(input, {
           ...init,
           headers: { ...init?.headers, 'X-User-Timezone': timeZone },
-          next: { revalidate: 60 },
-          cache: 'default'
+          // next: { revalidate: 60 },
+          cache: 'default',
         });
     },
   }));
