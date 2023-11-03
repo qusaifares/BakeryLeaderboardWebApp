bf6c591023c9696554628f43bbe264c011d1f0a6
Qusai Fares <qusaifares@gmail.com>
2023-11-02 20:38:51 -0700

    Update headers config add headers

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
         headers: [
           { key: 'Cache-Control', value: 'public, max-age=0, must-revalidate' },
           { key: 'CDN-Cache-Control', value: 'public, max-age=0, must-revalidate' },
-          { key: 'Vercel-CDN- Cache-Control', value: 'public, max-age=0, must-revalidate' },
+          { key: 'Vercel-CDN-Cache-Control', value: 'public, max-age=0, must-revalidate' },
         ]
       }]
   },
